dfee3a946f168563f9fc22e32a53592c0235d01c
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-03-16 13:49:18 +0530

    <*/*>?

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,6 +65,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
 }
 publishing {
     publications {
