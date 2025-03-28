238e253f87f4376e300b05c42ae0f8713bbabf5b
rajeev-k47 <rajeevkumawat2007@outlook.com>
2025-03-29 03:32:26 +0530

    <Init,Refac:Init reminder class and functions>?

---
+++ app/src/main/java/net/runner/r_aul/ReminderManager.kt
@@ -0,0 +1,44 @@
+package net.runner.r_aul
+
+import android.content.Context
+
+class ReminderManager {
+    companion object {
+        fun initReminder(context: Context,reminders:Int,githubVersion:String) {
+            val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+            val editor = sharedPreferences.edit()
+            editor.putString(githubVersion,reminders.toString())
+            editor.apply()
+        }
+        fun reminderExists(context: Context,githubVersion:String): Boolean {
+            val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+            return sharedPreferences.contains(githubVersion)
+        }
+        fun removeReminder(context: Context,githubVersion:String) {
+            val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+            val current = sharedPreferences.getString(githubVersion,"")
+            val new = current?.toInt()?.minus(1)
+            if (new != null) {
+                val editor = sharedPreferences.edit()
+                if(new> -1){
+                    editor.putString(githubVersion,new.toString())
+                    editor.apply()
+                }
+            }
+        }
+        fun verifyReminder(context: Context,githubVersion:String): Boolean {
+            val sharedPreferences = context.getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+            val current = sharedPreferences.getString(githubVersion,"")
+            val new = current?.toInt()?.minus(1)
+            if (new != null) {
+                if(new!=0){
+                    return true
+                }else{
+                    return false
+                }
+            }
+            return false
+        }
+    }
+
+}
--- app/src/main/java/net/runner/r_aul/reminderManager.kt
+++
@@ -1,2 +0,0 @@
-package net.runner.r_aul
-
